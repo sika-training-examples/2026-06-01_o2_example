ceca8b0d48521de804408355fe8ab8a170f70810
Ondrej Sika <ondrej@ondrejsika.com>
2026-06-01 11:11:36 +0200

    feat(backend): Create backend app

---
+++ backend/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"os"
+
+	"github.com/ondrejsika/counter/pkg/server"
+	"github.com/ondrejsika/counter/version"
+)
+
+func main() {
+	os.Setenv("EXTRA_TEXT", "Hello O2")
+	os.Setenv("API_ONLY", "1")
+	version.Version = "o2-v1"
+	server.Server(server.ServerOptions{})
+}
